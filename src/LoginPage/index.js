be84687c95a66bfd29ccb9a4144d007d31bb99c2
nullbeep <andrewskylander@gmail.com>
2022-05-27 21:03:22 -0400

    icons and colors

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Apple from "./apple.png";
-
+import ID from "./account.png";
+import Pass from "./padlock.png";
 const Container = styled.div`
-  background-color: black; //change to your own color
+  background-color: #5470A6; //change to your own color
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -15,14 +16,19 @@ const Container = styled.div`
 const LogoIcon = styled.img`
   width: 100px; //change
   height: 100px; // change
-  margin-top: 300px; // change
+  margin-top: 5vw; // change
 `;
 
 const Title = styled.div`
-  color: white; //
-  font-size: 100px; //change
+  color: #A6C4FF; //
+  font-size: 5.5vw; //change
+  margin-bottom: 0.7vw;
 `;
-
+const IDIcon = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-bottom: 0.4vw;
+  `
 const Paragraph = styled.div`
   color: yellow;
   font-size: 24px;
@@ -32,6 +38,8 @@ function LoginPage() {
     <Container>
       <LogoIcon src={Apple} />
       <Title>LogIn</Title>
+      <IDIcon src={ID}/>
+      <IDIcon src={Pass}/>
       <Paragraph>Hello this is Andrew</Paragraph>
     </Container>
   );
