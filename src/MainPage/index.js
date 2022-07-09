fc05957e3df2d8db2cf2b416f2bfad0d0685703f
nullbeep <andrewskylander@gmail.com>
2022-07-08 21:39:54 -0400

    main page

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Apple from "./apple.png";
+import Settings from "./settings.png";
+
+const Container = styled.div`
+  background-color: #406754; 
+  height: 100vh;
+  align-items: center;
+`;
+const LogoIcon = styled.img`
+  width: 125px; //change
+  height: 125px; // change
+  margin-top: 5vw; // change
+`;
+
+const SettingsIcon = styled.img`
+width: 75px;
+height: 75px;
+
+filter: invert(91%) sepia(24%) saturate(3490%) hue-rotate(193deg)
+    brightness(90%) contrast(97%);
+`
 
 function MainPage() {
-  return <div>ddd</div>;
-}
+  
+return(
+<Container>
+      <LogoIcon src={Apple} />
+      <SettingsIcon src={Settings} />
+</Container>
+)
 
+}
 export default MainPage;
